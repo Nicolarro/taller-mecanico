841aa3f525a7b02f5ebfc7f0825dc58a0fafb825
Nicolas Larrosa <86521241+Nicolarro@users.noreply.github.com>
2024-12-26 08:32:04 -0300

    Crendo el proyecto

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import React from'react';
+import Menu from './Menu/Menu.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Menu />
+      <div className="App-body">
+        <h1>Taller Mecanico</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Menu/Menu.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Menu.css';
+
+function Menu() {
+  return (
+    <header className="App-header">
+      <p >Probando la UI </p>
+    </header>
+  )
+}
+
+
+export default Menu;
